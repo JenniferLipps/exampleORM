7eb6c5f3426aefc552820a81cdc68f558f417eeb
Jennifer Lipps <jjlipps@gmail.com>
2019-11-05 19:56:15 -0600

    first commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './Home.scss';
+import valueRequests from '../../Components/apiRequests/valueRequests';
 
 class Home extends Component {
+
+  state = {
+    displayValues: []
+  }
+
+  getValues = () => {
+    valueRequests.getValues().then((values) => {
+      let myNewValues = [...values];
+      this.setState({displayValues: myNewValues})
+    }).catch((error) => {
+      console.log("broken", error)
+    })
+  }
 
   render () {
     const testText = this.props.testText;
     return (
       <div className="Home">
           <h1 className="testTarget">{testText}</h1>
+          <button onClick = {this.getValues}>Click Me</button>
       </div>
     );
   }
